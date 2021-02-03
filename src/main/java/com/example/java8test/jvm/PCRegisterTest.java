6e808a07451de531b1b63248b0101f9fb2cf6822
向延琳 <yanlin.xiang@chinacreator.com>
2021-02-03 22:28:28 +0800

    栈结构demo

---
+++ src/main/java/com/example/java8test/jvm/PCRegisterTest.java
@@ -0,0 +1,13 @@
+package com.example.java8test.jvm;
+
+/**
+ * @author XiangYanLin
+ * @date 2021/2/3
+ */
+public class PCRegisterTest {
+    public static void main(String[] args) {
+        int i = 10;
+        int j = 20;
+        int k = i + j;
+    }
+}
