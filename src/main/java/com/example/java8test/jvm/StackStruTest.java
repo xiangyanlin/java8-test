650ed47b9f40ddf1d206e955c132b12f81b7e4b7
向延琳 <yanlin.xiang@chinacreator.com>
2021-02-03 21:49:22 +0800

    栈结构demo

---
+++ src/main/java/com/example/java8test/jvm/StackStruTest.java
@@ -0,0 +1,13 @@
+package com.example.java8test.jvm;
+
+/**
+ * @author XiangYanLin
+ * @date 2021/2/1
+ * 栈结构测试
+ */
+public class StackStruTest {
+    public static void main(String[] args) {
+        int i = 2 + 3;
+
+    }
+}
