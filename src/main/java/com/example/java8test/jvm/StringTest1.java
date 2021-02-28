cd028096276730177fa6b2ad9624611af0bedeb0
向延琳 <yanlin.xiang@chinacreator.com>
2021-02-28 15:16:41 +0800

    String的不变性

---
+++ src/main/java/com/example/java8test/jvm/StringTest1.java
@@ -0,0 +1,43 @@
+package com.example.java8test.jvm;
+
+/**
+ * String的不可变性
+ * @author XiangYanLin
+ * @date 2021/2/28
+ */
+public class StringTest1 {
+    public static void test1() {
+        // 字面量定义的方式，“abc”存储在字符串常量池中
+        String s1 = "abc";
+        String s2 = "abc";
+        System.out.println(s1 == s2);
+        s1 = "hello";
+        System.out.println(s1 == s2);
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println("----------------");
+    }
+
+    public static void test2() {
+        String s1 = "abc";
+        String s2 = "abc";
+        // 只要进行了修改，就会重新创建一个对象，这就是不可变性
+        s2 += "def";
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println("----------------");
+    }
+
+    public static void test3() {
+        String s1 = "abc";
+        String s2 = s1.replace('a', 'm');
+        System.out.println(s1);
+        System.out.println(s2);
+    }
+
+    public static void main(String[] args) {
+        test1();
+        test2();
+        test3();
+    }
+}
